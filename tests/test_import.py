ce31a0cdc3e863b722097e3a6220f3a14e800c43
Philip Rehberger <me@philiprehberger.com>
2026-03-16 16:09:26 -0500

    Release v0.1.5

---
+++ tests/test_import.py
@@ -0,0 +1,7 @@
+"""Basic import test."""
+
+
+def test_import():
+    """Verify the package can be imported."""
+    import philiprehberger_file_organizer
+    assert hasattr(philiprehberger_file_organizer, "__name__") or True
